6867a1e57a63ccc2dc53cd7ca930a7ed1f71a6d4
IsaiahFlax <flax.isaiah@gmail.com>
2020-03-12 10:32:09 -0600

    got the data

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/IsaiahFlax')
+    .then(res => res.json())
+    .then(data => this.setState({ data: data }))
+    .catch(err => console.log("error"));
+
+  }
+  render(){
+    return(
+      <div>{console.log(this.state.data)}</div>
+    )
+  }
 }
 
 export default App;
